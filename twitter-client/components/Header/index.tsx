d11a96cb12e0664c10189e5960a0da6b8e6f29a7
Arpan Dhama <arpandhama2001@gmail.com>
2024-06-16 11:25:31 +0530

    add incomplete header

--- twitter-client/components/Header/index.tsx
+++ twitter-client/components/Header/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import ProfilePhoto from "../ProfilePhoto";
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className="flex items-center h-14">
+        <div className="">
+          <ProfilePhoto height={40} width={40} />
+        </div>
+        <div className="flex justify-center w-full ml-[-40px]">
+          LOGO COMPONENT
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
--- twitter-client/components/ProfilePhoto/index.tsx
+++ twitter-client/components/ProfilePhoto/index.tsx
@@ -4,16 +4,18 @@ import Image from "next/image";
 interface ProfilePhotoProps {
   height?: number;
   width?: number;
+  src?: string;
 }
 
 const ProfilePhoto: React.FC<ProfilePhotoProps> = ({
   height = 50,
   width = 50,
+  src = "https://pbs.twimg.com/profile_images/1679899939445387284/Pf1EQs8L_400x400.png",
 }): JSX.Element => {
   return (
     <div className="flex justify-center lg:justify-start items-center px-2">
       <Image
-        src="https://pbs.twimg.com/profile_images/1679899939445387284/Pf1EQs8L_400x400.png"
+        src={src}
         alt="profile image"
         height={height}
         width={width}
